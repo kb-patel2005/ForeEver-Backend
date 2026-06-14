cea1c704c0e46ffb3baef61e8cdb8c8bd01aa929
kb-patel2005 <krishpatel1181@gmail.com>
2026-06-14 19:39:38 +0530

    origin access...

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors({
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://foreverweb.netlify.app/",
     methods: ["GET", "POST"]
   }
 });
